10be6be31a423820ad7161cce01dc57ea3f5f229
MLH84NKA <artm-dymovich@ya.ru>
2022-12-13 10:23:34 +0300

    index

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,31 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true
     },
   });
+
+ymaps.ready(init);
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [59.851422, 30.300983],
+        
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 13.5
+    });
+    var placemark = new ymaps.Placemark([59.851422, 30.300983], {},
+        {
+            preset: 'islands#icon',
+            iconColor: '#a40000'
+        }
+    );
+    myMap.geoObjects.add(placemark);
+}
+        
+      
+
+
+
